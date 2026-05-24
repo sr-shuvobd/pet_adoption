6fefa29e4dc6ac556e7444af33e122c90f6df0bf
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-05-24 23:31:52 +0600

    update adopt form

--- src/app/details/[id]/page.jsx
+++ src/app/details/[id]/page.jsx
@@ -1,228 +1,172 @@
+import AdoptForm from "@/components/AdoptForm";
+import Image from "next/image";
+import React from "react";
+import { CiLocationOn } from "react-icons/ci";
+import { FaDollarSign } from "react-icons/fa";
+import { MdHealthAndSafety } from "react-icons/md";
+import { TbVaccine } from "react-icons/tb";
 
-import React from 'react';
+const DetailsPage = async ({ params }) => {
+  const { id } = await params;
 
-const DetailsPage = async({params}) => {
-  const {id} = await params;
+  const res = await fetch(`http://localhost:5000/allpet/${id}`);
+  const pet = await res.json();
+  const {
+    _id,
+    imageUrl,
+    petName,
+    species,
+    age,
+    adoptionFee,
+    healthStatus,
+    vaccinationStatus,
+    location,
+    description,
+    breed,
+    gender,
+  } = pet;
+  return (
+    <div className="py-14 px-5 grid grid-cols-1 md:grid-cols-2 gap-15 bg-[#D68B6E]">
+      <div className="bg-gray-200 rounded-2xl p-5 shadow">
+        {/* Image Section */}
+        <div className="relative group overflow-hidden rounded-2xl">
+          <Image
+            src={imageUrl}
+            alt={petName}
+            width={500}
+            height={500}
+            className="
+                w-full
+                h-full
+                object-cover
+                lg:h-[400px]
+                rounded-2xl
+                group-hover:scale-110
+                transition-all duration-700
+              "
+          />
 
-    const res = await fetch(`http://localhost:5000/allpet/${id}`);
-    const pet = await res.json();
-    const { _id, imageUrl, petName, species, age, adoptionFee,healthStatus,vaccinationStatus,location,description, breed,gender} = pet;
-    return (
-        <div className="py-14 px-5">
-
-  <div
-    className="
-      bg-white
-      rounded-[35px]
-      shadow-2xl
-      overflow-hidden
-      grid grid-cols-1 lg:grid-cols-2
-    "
-  >
-
-    {/* Image Section */}
-    <div className="relative group overflow-hidden">
-
-      <img
-        src={imageUrl}
-        alt={petName}
-        className="
-          w-full
-          h-full
-          object-cover
-          lg:h-[800px]
-          group-hover:scale-110
-          transition-all duration-700
-        "
-      />
-
-      {/* Overlay */}
-      <div
-        className="
+          <div
+            className="
           absolute inset-0
           bg-gradient-to-t
           from-black/70
           to-transparent
         "
-      ></div>
+          ></div>
 
-      {/* Pet Name On Image */}
-      <div className="absolute bottom-10 left-10">
-
-        <h1
-          className="
+          {/* Pet Name On Image */}
+          <div className="absolute bottom-10 left-10">
+            <h1
+              className="
             text-5xl
             font-extrabold
             text-white
             drop-shadow-lg
           "
-        >
-          {petName}
-        </h1>
+            >
+              {petName}
+            </h1>
 
-        <p className="text-[#F5DCD1] text-xl mt-2">
-          {species} • {breed}
-        </p>
+            <p className="text-[#F5DCD1] text-xl mt-2">
+              {species} • {breed}
+            </p>
+          </div>
+        </div>
+        {/* Details Section */}
+        <div className="p-8 lg:p-6 flex flex-col justify-center">
+          {/* Top Badge */}
 
-      </div>
+          <div className="flex gap-3 mb-2 flex-wrap justify-between">
+            <div className="flex gap-3 items-center">
+              <span
+                className="
+                    px-5 py-2
+                    rounded-full
+                    bg-[#064E3B]
+                    text-white
+                    text-sm
+                    font-semibold
+                  "
+              >
+                {gender}
+              </span>
 
-    </div>
+              <span
+                className="
+                    px-5 py-2
+                    rounded-full
+                    bg-[#D68B6E]
+                    text-white
+                    text-sm
+                    font-semibold
+                  "
+              >
+                {age} Years Old
+              </span>
+            </div>
+            <span className="flex flex-col text-center">
+              <p className="text-gray-500">Adoption Fee</p>
+              <h1 className="text-2xl text-[#D68B6E]">${adoptionFee}</h1>
+            </span>
+          </div>
 
-    {/* Details Section */}
-    <div className="p-8 lg:p-12 flex flex-col justify-center">
-
-      {/* Top Badge */}
-      <div className="flex gap-3 mb-6 flex-wrap">
-
-        <span
-          className="
-            px-5 py-2
-            rounded-full
-            bg-[#064E3B]
-            text-white
-            text-sm
-            font-semibold
-          "
-        >
-          {gender}
-        </span>
-
-        <span
-          className="
-            px-5 py-2
-            rounded-full
-            bg-[#D68B6E]
-            text-white
-            text-sm
-            font-semibold
-          "
-        >
-          {age} Years Old
-        </span>
-
-      </div>
-
-      {/* Title */}
-      <h2
-        className="
+          {/* Title */}
+          <h2
+            className="
           text-4xl
           font-extrabold
           text-[#064E3B]
           leading-tight
         "
-      >
-        Meet {petName}
-      </h2>
+          >
+            Meet {petName}
+          </h2>
 
-      {/* Description */}
-      <p
-        className="
-          text-gray-600
-          leading-8
-          mt-6
-          text-lg
-        "
-      >
-        {description}
-      </p>
-
-      {/* Info Cards */}
-      <div
-        className="
-          grid grid-cols-2
-          gap-5
-          mt-10
-        "
-      >
-
-        <div className="bg-[#f8f8f8] p-5 rounded-2xl">
-          <h3 className="text-gray-500 text-sm">
-            Adoption Fee
-          </h3>
-
-          <p className="text-2xl font-bold text-[#064E3B] mt-2">
-            ${adoptionFee}
+          {/* Description */}
+          <p
+            className="
+                text-gray-600
+                leading-8
+                text-lg
+              "
+          >
+            {description}
           </p>
+
+          {/* card */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+            <div className="but p-3 bg-[#064E3B] text-white rounded">
+              <p className="text-sm text-gray-300 mb-2">Health Status</p>
+              <h1 className="flex gap-2 items-center"><MdHealthAndSafety />{healthStatus}</h1>
+              
+            </div>
+
+            <div className="but p-3 bg-[#064E3B] text-white rounded">
+              <p className="text-sm text-gray-300 mb-2">Vaccirination Status</p>
+              <h1 className="flex gap-2 items-center"><TbVaccine />{vaccinationStatus}</h1>
+            </div>
+
+             <div className="but p-3 bg-[#064E3B] text-white rounded">
+              <p className="text-sm text-gray-300 mb-2">Location</p>
+              <h1 className="flex gap-2 items-center"><CiLocationOn />{location}</h1>
+            </div>
+
+            <div className="but p-3 bg-[#064E3B] text-white rounded">
+              <p className="text-sm text-gray-300 mb-2">Adoption Fee</p>
+              <h1 className="flex gap-2 items-center"><FaDollarSign />{adoptionFee}</h1>
+            </div>
+
+          </div>
         </div>
-
-        <div className="bg-[#f8f8f8] p-5 rounded-2xl">
-          <h3 className="text-gray-500 text-sm">
-            Location
-          </h3>
-
-          <p className="text-2xl font-bold text-[#064E3B] mt-2">
-            {location}
-          </p>
-        </div>
-
-        <div className="bg-[#f8f8f8] p-5 rounded-2xl">
-          <h3 className="text-gray-500 text-sm">
-            Health Status
-          </h3>
-
-          <p className="text-xl font-bold text-[#064E3B] mt-2">
-            {healthStatus}
-          </p>
-        </div>
-
-        <div className="bg-[#f8f8f8] p-5 rounded-2xl">
-          <h3 className="text-gray-500 text-sm">
-            Vaccination
-          </h3>
-
-          <p className="text-xl font-bold text-[#064E3B] mt-2">
-            {vaccinationStatus}
-          </p>
-        </div>
-
       </div>
 
-      {/* Buttons */}
-      <div className="flex gap-5 mt-12 flex-wrap">
-
-        <button
-          className="
-            px-10 py-4
-            rounded-full
-            bg-[#064E3B]
-            text-white
-            font-bold
-            text-lg
-            hover:bg-[#0c6d52]
-            hover:scale-105
-            transition-all duration-300
-            shadow-lg
-          "
-        >
-          Adopt Now
-        </button>
-
-        <button
-          className="
-            px-10 py-4
-            rounded-full
-            border-2 border-[#D68B6E]
-            text-[#D68B6E]
-            font-bold
-            text-lg
-            hover:bg-[#D68B6E]
-            hover:text-white
-            hover:scale-105
-            transition-all duration-300
-          "
-        >
-          Contact Owner
-        </button>
-
+      {/* Form Section  */}
+      <div className="shadow bg-gray-200 rounded-2xl p-10">
+        <AdoptForm pet={pet} />
       </div>
-
     </div>
-
-  </div>
-
-</div>
-    );
+  );
 };
 
 export default DetailsPage;
-
---
+++ src/components/AdoptForm.jsx
@@ -0,0 +1,137 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextArea,
+  TextField,
+} from "@heroui/react";
+import React from "react";
+
+const AdoptForm = ({ pet }) => {
+  const {
+    _id,
+    imageUrl,
+    petName,
+    species,
+    age,
+    adoptionFee,
+    healthStatus,
+    vaccinationStatus,
+    location,
+    description,
+    breed,
+    gender,
+  } = pet;
+
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+
+  const onSubmit = async(e) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+
+    const allData = {
+      ...data,
+      userId: user.id,
+      id: _id,
+      status: "pending"
+    }
+    console.log(data)
+
+     await fetch("http://localhost:5000/adoptpet", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(allData),
+    });
+  }
+
+  return (
+    <div>
+      <Form onSubmit={onSubmit} className="space-y-2">
+        <h1 className="text-3xl mb-5 font-bold text-[#D68B6E]">
+          Request to adopt {species}
+        </h1>
+        <TextField
+          defaultValue={petName}
+          isRequired
+          isReadOnly
+          name="petname"
+          type="text"
+        >
+          <Label>Pet Name</Label>
+          <Input />
+          <FieldError />
+        </TextField>
+        <TextField
+          defaultValue={user?.name}
+          isRequired
+          isReadOnly
+          name="username"
+          type="text"
+        >
+          <Label>Your Name</Label>
+          <Input />
+          <FieldError />
+        </TextField>
+        <TextField
+          defaultValue={user?.email}
+          isRequired
+          isReadOnly
+          name="useremail"
+          type="text"
+        >
+          <Label>Your Email</Label>
+          <Input />
+          <FieldError />
+        </TextField>
+
+        <TextField isRequired name="date" type="date">
+          <Label>Preferred Pickup Date</Label>
+          <Input />
+          <FieldError />
+        </TextField>
+
+        <TextField name="message">
+          <Label>Message to Owner</Label>
+          <TextArea
+            placeholder="Write a short message to the pet owner..."
+            minRows={6}
+          />
+          <FieldError />
+        </TextField>
+
+        <div className="flex flex-col gap-5 text-center">
+          <Button
+            variant="outline"
+            type="submit"
+            className=" btn px-5 py-2 flex items-center gap-2 mt-5
+                rounded
+                border-2 border-[#D68B6E]
+                text-[#D68B6E]
+                font-semibold
+                hover:bg-[#D68B6E]
+                hover:text-white
+                transition-all
+                duration-300
+                shadow-[0_0_15px_rgba(214,139,110,0.4)]
+                hover:scale-105"
+          >
+            <Check />
+            Adopt Request
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default AdoptForm;
