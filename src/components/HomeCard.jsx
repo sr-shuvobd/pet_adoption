9c6dbbfd1670b1888ff72a318ce3b12b3584f5b4
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-05-26 17:22:43 +0600

    card sektion update

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,4 +1,5 @@
-import { Button } from "@heroui/react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -6,27 +7,43 @@ import { FaEye } from "react-icons/fa";
 
 const HomeCard = async () => {
   const res = await fetch("http://localhost:5000/allpet");
-
   const pets = await res.json();
+
+  const requestRes = await fetch("http://localhost:5000/adoptpet");
+  const requests = await requestRes.json();
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const user = session?.user;
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-7">
       {pets.slice(0, 6).map((pet) => {
-        const { _id, imageUrl, petName, species, age, adoptionFee } = pet;
+        const { _id, imageUrl, petName, species, age, adoptionFee, userId } =
+          pet;
+
+        const isOwner = userId === user?.id;
+
+        const isConfirmed = requests.find(
+          (item) => item.id === _id && item.status === "confirm",
+        );
 
         return (
           <div
             key={_id}
             className="
-              bg-white
-              rounded-2xl
-              overflow-hidden
-              shadow-md
-              hover:shadow-2xl
-              transition-all duration-300
-              hover:-translate-y-2
-              group
-            "
+                bg-white
+                rounded-2xl
+                overflow-hidden
+                shadow-md
+                hover:shadow-2xl
+                transition-all
+                duration-300
+                hover:-translate-y-2
+                group
+              "
           >
             <div className="overflow-hidden relative">
               <Image
@@ -35,30 +52,40 @@ const HomeCard = async () => {
                 width={500}
                 height={500}
                 className="
-                  w-full
-                  h-64
-                  object-cover
-                  group-hover:scale-110
-                  transition-all duration-500
-                "
+                    w-full
+                    h-64
+                    object-cover
+                    group-hover:scale-110
+                    transition-all
+                    duration-500
+                  "
               />
+
               <h1
-                className="absolute
-                            top-4
-                            right-4
-                            bg-black/40
-                            backdrop-blur-sm
-                            text-green-500
-                            px-4
-                            py-2
-                            rounded-full
-                            text-sm
-                            font-semibold
-                            border border-white/20"
-                >
-                Available
+                className={`
+                    absolute
+                    top-4
+                    right-4
+                    px-4
+                    py-2
+                    rounded-full
+                    text-sm
+                    font-semibold
+                    border
+                    border-white/20
+                    backdrop-blur-sm
+
+                    ${
+                      isConfirmed
+                        ? "bg-red-500/70 text-white"
+                        : "bg-black/40 text-green-400"
+                    }
+                  `}
+              >
+                {isConfirmed ? "Adopted" : "Available"}
               </h1>
             </div>
+
             <div className="p-5">
               <h1 className="text-2xl font-bold text-[#064E3B] mb-2">
                 {petName}
@@ -82,27 +109,42 @@ const HomeCard = async () => {
               <div className="flex gap-3 mt-5">
                 <Link
                   href={`/details/${_id}`}
-                  className="btn rounded-full flex-1 w-full
+                  className="
+                      btn
+                      rounded-full
+                      flex-1
+                      w-full
                       bg-[#064E3B]
                       text-white
-                      hover:bg-[#0b6b50]"
+                      hover:bg-[#0b6b50]
+                    "
                 >
-                  {" "}
                   <FaEye />
                   View Details
                 </Link>
 
                 <Link
                   href={`/adoptform/${_id}`}
-                  className="flex-1 w-full btn rounded-full
+                  className={`
+                      flex-1
+                      w-full
+                      btn
+                      rounded-full
                       border-2
                       border-[#D68B6E]
                       text-[#D68B6E]
                       bg-transparent
                       hover:bg-[#D68B6E]
-                      hover:text-white"
+                      hover:text-white
+
+                      ${
+                        isOwner || isConfirmed
+                          ? "pointer-events-none opacity-50"
+                          : ""
+                      }
+                    `}
                 >
-                  Adopt
+                  {isOwner ? "Your Pet" : isConfirmed ? "Adopted" : "Adopt"}
                 </Link>
               </div>
             </div>
--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -1,4 +1,5 @@
-import { Button } from "@heroui/react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -6,42 +7,83 @@ import { FaEye } from "react-icons/fa";
 
 const PetCard = async () => {
   const res = await fetch("http://localhost:5000/allpet");
+
   const pets = await res.json();
+
+  const requestRes = await fetch("http://localhost:5000/adoptpet");
+
+  const requests = await requestRes.json();
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const user = session?.user;
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {pets.map((pet) => {
-        const { _id, imageUrl, petName, species, age, adoptionFee } = pet;
+        const { _id, imageUrl, petName, species, age, adoptionFee, userId } =
+          pet;
+
+        const isOwner = userId === user?.id;
+
+        const isConfirmed = requests.find(
+          (item) => item.id === _id && item.status === "confirm",
+        );
 
         return (
           <div
             key={_id}
             className="
-              bg-white
-              rounded-2xl
-              overflow-hidden
-              shadow-md
-              hover:shadow-2xl
-              transition-all duration-300
-              hover:-translate-y-2
-              group
-            "
+                bg-white
+                rounded-2xl
+                overflow-hidden
+                shadow-md
+                hover:shadow-2xl
+                transition-all duration-300
+                hover:-translate-y-2
+                group
+              "
           >
-            <div className="overflow-hidden">
+            <div className="overflow-hidden relative">
               <Image
                 src={imageUrl || "/assets/heroimage.png"}
                 alt={petName}
                 width={500}
                 height={500}
                 className="
-                  w-full
-                  h-64
-                  object-cover
-                  group-hover:scale-110
-                  transition-all duration-500
-                "
+                    w-full
+                    h-64
+                    object-cover
+                    group-hover:scale-110
+                    transition-all duration-500
+                  "
               />
+
+              <h1
+                className={`
+                    absolute
+                    top-4
+                    right-4
+                    px-4
+                    py-2
+                    rounded-full
+                    text-sm
+                    font-semibold
+                    backdrop-blur-sm
+
+                    ${
+                      isConfirmed
+                        ? "bg-red-500/80 text-white"
+                        : "bg-black/40 text-green-400"
+                    }
+                  `}
+              >
+                {isConfirmed ? "Adopted" : "Available"}
+              </h1>
             </div>
+
             <div className="p-5">
               <h1 className="text-2xl font-bold text-[#064E3B] mb-2">
                 {petName}
@@ -63,23 +105,44 @@ const PetCard = async () => {
               </div>
 
               <div className="flex gap-3 mt-5">
-                <Link href={`/details/${_id}`} className="btn rounded-full flex-1 w-full
+                <Link
+                  href={`/details/${_id}`}
+                  className="
+                      btn
+                      rounded-full
+                      flex-1
+                      w-full
                       bg-[#064E3B]
                       text-white
-                      hover:bg-[#0b6b50]">
-                    {" "}
-                    <FaEye />
-                    View Details
+                      hover:bg-[#0b6b50]
+                    "
+                >
+                  <FaEye />
+                  View Details
                 </Link>
 
-                <Link href={`/adoptform/${_id}`} className="flex-1 w-full btn rounded-full
+                <Link
+                  href={`/adoptform/${_id}`}
+                  className={`
+                      flex-1
+                      w-full
+                      btn
+                      rounded-full
                       border-2
                       border-[#D68B6E]
                       text-[#D68B6E]
                       bg-transparent
                       hover:bg-[#D68B6E]
-                      hover:text-white">
-                    Adopt
+                      hover:text-white
+
+                      ${
+                        isOwner || isConfirmed
+                          ? "pointer-events-none opacity-50"
+                          : ""
+                      }
+                    `}
+                >
+                  {isOwner ? "Your Pet" : isConfirmed ? "Adopted" : "Adopt"}
                 </Link>
               </div>
             </div>
