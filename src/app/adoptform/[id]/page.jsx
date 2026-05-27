81d7e4c3303d72f5692d13834e06e977393ef65b
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-05-27 22:32:19 +0600

    slove problem

--- src/app/adoptform/[id]/page.jsx
+++ src/app/adoptform/[id]/page.jsx
@@ -1,11 +1,21 @@
 import AdoptForm from '@/components/AdoptForm';
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import React from 'react';
 
 const Adopt = async ({params}) => {
     const { id } = await params;
 
-  const res = await fetch(`${process.env.NEXT_PUBLICK_URL}/allpet/${id}`);
-  const pet = await res.json();
+    const { token } = await auth.api.getToken({
+        headers: await headers(),
+    });
+
+    const res = await fetch(`${process.env.NEXT_PUBLICK_URL}/allpet/${id}`, {
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+    });
+    const pet = await res.json();
     return (
         <div className='h-screen flex justify-center items-center bg-[#FFF1EB]'>
             <div className='bg-white p-10 rounded-2xl'>
